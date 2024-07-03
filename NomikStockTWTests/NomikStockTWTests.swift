955a04ad68cebdd15d2811f2b985f44670bf8bab
NomikLee <nomiklee@icloud.com>
2024-07-03 08:57:46 +0800

    Initial Commit

---
+++ NomikStockTWTests/NomikStockTWTests.swift
@@ -0,0 +1,17 @@
+//
+//  NomikStockTWTests.swift
+//  NomikStockTWTests
+//
+//  Created by Pinocchio on 2024/7/3.
+//
+
+import Testing
+@testable import NomikStockTW
+
+struct NomikStockTWTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
